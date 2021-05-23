95441808e4e727fd8a0b3568d5ae076019914d52
zhang <zhangpei@bupt.edu.cn>
2021-05-23 20:22:22 +0800

    update dir

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import IPy
+from IPy import IP
+import ipaddress
+#print(len(list(ipaddress.ip_network('2001:fd0::/36').subnets(new_prefix=36))))
+#print(2**12)
+import os
+from itertools import islice
+print(IP('240e:4:e000::/40') in IP('240e:2000::/19'))
+
+
+
+
+
